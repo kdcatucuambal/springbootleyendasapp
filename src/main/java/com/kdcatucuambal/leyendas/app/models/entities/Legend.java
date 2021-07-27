eefac8d0352fcf8aec74547b826692d8bb6c9e29
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2021-07-27 09:30:14 -0500

    trying developer branch

---
+++ src/main/java/com/kdcatucuambal/leyendas/app/models/entities/Legend.java
@@ -0,0 +1,11 @@
+package com.kdcatucuambal.leyendas.app.models.entities;
+
+public class Legend {
+	
+	private String id;
+	private String title;
+	private String fragment;
+	private String content;
+	private String country;
+
+}
