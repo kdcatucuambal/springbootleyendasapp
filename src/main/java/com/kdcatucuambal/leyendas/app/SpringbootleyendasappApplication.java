6137035cdd02467f3cb9631ebc1586d0ddc103b2
Kevin Catucuamba <kdcatucuambal@utn.edu.ec>
2021-07-27 09:22:21 -0500

    app setting

---
+++ src/main/java/com/kdcatucuambal/leyendas/app/SpringbootleyendasappApplication.java
@@ -0,0 +1,13 @@
+package com.kdcatucuambal.leyendas.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootleyendasappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootleyendasappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdcatucuambal/springboot/app/SpringbootleyendasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdcatucuambal.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootleyendasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
